951ed775eb7eceb1ffa7a13618295125ff00fccd
TianyiZhao1437 <tim@gradient.network>
2025-10-29 06:33:23 +0000

    update

--- amago/agent.py
+++ amago/agent.py
@@ -440,11 +440,36 @@ class Agent(nn.Module):
                 - Updated hidden state of the TrajEncoder.
         """
 
+        # 1. tstep_emb
         tstep_emb = self.tstep_encoder(obs=obs, rl2s=rl2s)
+
+        # 2. traj_encoder
         # sequence model embedding [batch, length, d_emb]
         traj_emb_t, hidden_state = self.traj_encoder(
             tstep_emb, time_idxs=time_idxs, hidden_state=hidden_state
         )
+        traj_encoder_input = {
+            'seq': tstep_emb,
+            'time_idxs': time_idxs,
+        }
+        traj_encoder_onnx = 'traj_encoder.onnx'
+        torch.onnx.export(
+            self.traj_encoder,
+            traj_encoder_input,
+            traj_encoder_onnx,
+            opset_version=10,
+            do_constant_folding=True,
+            input_names=["seq", "time_idxs"],
+            output_names=["traj_emb_t", "hidden_state"],
+            dynamic_axes={
+                "seq":{0:"batch_size", 1:"seq_length"},
+                "time_idxs":{0:"batch_size", 1:"seq_length"}, 
+                "traj_emb_t":{0:"batch_size", 1:"seq_length"},
+                "hidden_state":{0:"batch_size", 1:"seq_length"},
+            },
+        )
+
+        # 3. actor
         # generate action distribution [batch, length, len(self.gammas), d_action]
         action_dists = self.actor(
             traj_emb_t,
