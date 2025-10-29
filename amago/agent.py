70aaefdc209c198a64dde181465cf330d7baf670
TianyiZhao1437 <tim@gradient.network>
2025-10-29 07:16:37 +0000

    update

--- amago/agent.py
+++ amago/agent.py
@@ -445,14 +445,13 @@ class Agent(nn.Module):
 
         # 2. traj_encoder
         # sequence model embedding [batch, length, d_emb]
-        traj_emb_t, hidden_state = self.traj_encoder(
-            tstep_emb, time_idxs=time_idxs, hidden_state=hidden_state
-        )
-        traj_encoder_input = (tstep_emb, time_idxs, hidden_state)
+        # traj_emb_t, hidden_state = self.traj_encoder(
+        #     tstep_emb, time_idxs=time_idxs, hidden_state=hidden_state
+        # )
+        traj_encoder_input = (tstep_emb, time_idxs)
         dynamic_shapes = {
             "seq": {0: "batch_size", 1: "seq_length"},
             "time_idxs": {0: "batch_size", 1: "seq_length"},
-            "hidden_state": {0: "batch_size", 1: "seq_length"},
         }
         traj_encoder_onnx = 'traj_encoder.onnx'
         torch.onnx.export(
@@ -460,12 +459,15 @@ class Agent(nn.Module):
             traj_encoder_input,
             traj_encoder_onnx,
             do_constant_folding=True,
-            input_names=["seq", "time_idxs", "hidden_state_in"],
-            output_names=["traj_emb_t", "hidden_state_out"],
+            input_names=["seq", "time_idxs"],
+            output_names=["traj_emb_t"],
             dynamic_shapes=dynamic_shapes,
         )
 
         # 3. actor
+        traj_emb_t, hidden_state = self.traj_encoder(
+            tstep_emb, time_idxs=time_idxs
+        )
         # generate action distribution [batch, length, len(self.gammas), d_action]
         action_dists = self.actor(
             traj_emb_t,
