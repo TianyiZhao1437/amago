2e24224b331d1418c566d8d3a0f99e2517e0405b
TianyiZhao1437 <tim@gradient.network>
2025-11-19 11:25:41 +0000

    update

--- amago/nets/transformer.py
+++ amago/nets/transformer.py
@@ -100,8 +100,9 @@ class VanillaAttention(SelfAttention):
         cache_idxs = torch.arange(key_cache.shape[0], device=key_cache.device)
         # key_cache[cache_idxs, seq_lens] = keys[:, 0]
         # val_cache[cache_idxs, seq_lens] = values[:, 0]
-        k_cache = key_cache[cache_idxs].index_add(1, seq_lens, keys[:, 0])
-        v_cache = val_cache[cache_idxs].index_add(1, seq_lens, values[:, 0])
+        # [1, 128, 20, 64] + [1, 20, 64]
+        k_cache = key_cache[cache_idxs].index_add(0, seq_lens, torch.unsqueeze(keys[:, 0], 0))
+        v_cache = val_cache[cache_idxs].index_add(0, seq_lens, torch.unsqueeze(values[:, 0], 0))
 
         # max_len = seq_lens + 1
         # # max_len = end.max()
